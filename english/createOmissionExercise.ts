a58e74ef5088b60fbd12b47e2b9e191e90a71dab
Vitaly Likhachev <vitaly99878@gmail.com>
2024-12-17 12:47:24 +0300

    feat createOmissionExercise. Сделал создание упражнения для фронта из Учебника Голицынского.

--- english/createOmissionExercise.ts
+++ english/createOmissionExercise.ts
@@ -11,7 +11,7 @@ function createOmissionExercise() {
 
   fs.readFile(`${currentDirectory}/english/${exPath}`, 'utf8', (err, data) => {
     if (err) {
-      console.log('File does not exist');
+      return console.log('File does not exist');
     } else {
       exercise += data;
     }
@@ -22,16 +22,44 @@ function createOmissionExercise() {
     'utf8',
     (err, data) => {
       if (err) {
-        console.log('File does not exist');
+        return console.log('File does not exist');
       } else {
         executedExercise += data;
+        createExercise(exercise, executedExercise);
       }
-
-      // todo создать Упражение здесь
-      // console.log('exercise: ', exercise);
-      // console.log('executedExercise: ', executedExercise);
     }
   );
+}
+
+function createExercise(executed: string, executedExercise: string): string {
+  const placeholder = '…';
+
+  // Разделить тексты на массивы слов
+  const words = executed.replace(/\n/g, ' ').split(/\s+/);
+  const wordsExecuted = executedExercise.replace(/\n/g, ' ').split(/\s+/);
+
+  words.forEach((w, index) => {
+    const condition =
+      w === placeholder && wordsExecuted[index + 1] !== words[index + 1];
+    if (condition) {
+      wordsExecuted.splice(index, 0, '[ ]');
+    }
+  });
+
+  let index = 0;
+  const resultWords = wordsExecuted.map((word) => {
+    // Если слово в Тексте 1 является "…" (или это пробел после "…"), выделяем "похожее" из Текста 2
+    if (words[index] === placeholder) {
+      index++;
+      return word !== '[ ]' ? `[${word}]` : '[ ]';
+    }
+    // Иначе проверяем текущее слово и продолжаем дальше
+    index++;
+    return word;
+  });
+
+  // Соединяем обработанный текст
+  return resultWords.join(' ');
 }
 
 createOmissionExercise();
